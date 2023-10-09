a7d06432beb531442861f563c2a991b3c3fcd0b4
George Papeorgiou <george@Dias.home>
2023-10-09 09:55:54 +0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import discord
+from discord.ext import commands,tasks
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+DISCORD_TOKEN = os.getenv("discord_token")
+
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!',intents=intents)
+
+tokenFile = open("token", "r")
+TOKEN = tokenFile.read()
+
+
+@bot.command(name='p', help='To play song')
+async def play(ctx):
+
+    try :
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+
+        async with ctx.typing():
+            print("got in")
+            voice_channel.play(discord.FFmpegPCMAudio('https://stream.radioparadise.com/flac'))
+        await ctx.send('**Thank You For Tuning In**')
+    except:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@bot.command(name='j', help='Tells the bot to join the voice channel')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+@bot.command(name='l', help='To make the bot leave the voice channel')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+@bot.event
+async def on_ready():
+    print('Running!')
+    for guild in bot.guilds:
+        for channel in guild.text_channels :
+            if str(channel) == "general" :
+                await channel.send('Bot Activated!')
+        print('Active in {}\n Member Count : {}'.format(guild.name,guild.member_count))
+
+
+bot.run(TOKEN)
